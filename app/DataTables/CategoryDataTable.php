8e9f3dd596057b373bfa87eb0b75d45ac8dc09a7
Samuel Makoholi <samuelmakoh@gmail.com>
2024-01-19 17:15:01 +0200

    change status

--- app/DataTables/CategoryDataTable.php
+++ app/DataTables/CategoryDataTable.php
@@ -30,12 +30,15 @@ class CategoryDataTable extends DataTable
             ->addColumn('status', function($query){
 
             if($query->status == 1){
-
-                $button = '<label class="custom-switch mt-2">
-                <input type="checkbox" checked name="custom-switch-checkbox"  class="custom-switch-input">
+                $button ='<label class="custom-switch mt-2">
+                <input type="checkbox" checked name="custom-switch-checkbox" data-id="'.$query->id.'" class="custom-switch-input change-status">
                 <span class="custom-switch-indicator"></span>
               </label>';
-
+            }else{
+                $button ='<label class="custom-switch mt-2">
+                <input type="checkbox" name="custom-switch-checkbox" data-id="'.$query->id.'" class="custom-switch-input change-status">
+                <span class="custom-switch-indicator"></span>
+              </label>';
             }
              
             return $button;
